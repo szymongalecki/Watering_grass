0ed75cc029486ca66a96ca02a8bc1514c1b3e2a4
Szymon Gałecki <szymongalecki@gmail.com>
2022-12-13 14:38:49 +0100

    cleaned version

--- wg.py
+++ wg.py
@@ -17,7 +17,6 @@ import math
 while True:
     try:
         n, end, w = [int(_) for _ in input().split()]
-        # print(n, l, w)
         sprinkler = dict()
         coverage = list()
     except (EOFError, ValueError):
@@ -27,53 +26,16 @@ while True:
         # sprinkler's position and radius
         x, r = [int(_) for _ in input().split()]
 
-        # don't add useless sprinkler
+        # useless sprinklers are excluded
         if 2 * r <= w:
             continue
 
         # covered rectangular area, left and right from position
         k = math.sqrt(math.pow(r, 2) - math.pow(w / 2, 2))
         coverage.append([x - k, x + k])
-        sprinkler[x] = r
 
     # sort by left side coverage
     coverage.sort()
-
-    # # start with no sprinklers, on the left side of the strip
-    # s = 1
-    # left, right = coverage.pop(0)
-    # if left > 0:
-    #     print(-1)
-    #     break
-
-    # # choose the sprinkler overlapping with previous one, with the biggest coverage
-    # while coverage:
-
-    #     # end was reached, finish
-    #     if right >= end:
-    #         print(s)
-    #         break
-
-    #     # choose compatible sprinklers, do not include yourself
-    #     compatible = [
-    #         [l, r] for l, r in coverage if l <= right and l != left and r != right
-    #     ]
-
-    #     # detect gap and finish or proceed
-    #     if compatible:
-    #         left, right = max(compatible, key=lambda x: x[1])
-    #         f = coverage.index([left, right])
-    #         coverage = coverage[f:]
-    #         s += 1
-    #     else:
-    #         print("-1")
-    #         break
-
-    # trivial case
-    if coverage[0][0] <= 0 and coverage[0][1] >= end:
-        # print(f"Right side of the grass strip was reached using 1 sprinkler")
-        print(1)
-        continue
 
     # parameters
     s = 1
@@ -81,61 +43,75 @@ while True:
     l_best, r_best = 0, 0
 
     for i, (l, r) in enumerate(coverage):
-        # first sprinkler can covers the left side of grass strip
+        # previously used sprinkler reached the end of grass strip
+        if r_used > end:
+            print(s)
+            break
+
+        # first sprinkler can't cover the left side of grass strip
         if i == 0 and l > 0:
-            # print("Left side of the grass strip can't be covered")
             print(-1)
             break
 
-        # check if last sprinkler can cover the right side of the grass strip
+        # last sprinkler can't cover the right side of grass strip
         if i == len(coverage) - 1 and r < end:
-            # print("Right side of the grass strip can't be reached")
             print(-1)
             break
 
-        # check if current sprinkler is compatible with previously used sprinkler
+        # current sprinkler is compatible with previously used sprinkler
         if l <= r_used:
             if r > end:
-                # print(
-                #     f"Right side of the grass strip was reached using {s + 1} sprinklers"
-                # )
                 print(s + 1)
                 break
 
-            # check if it covers the most area to the right
+            # current sprinkler has the biggest right side coverage
             if r > r_best:
-                # the best from compatible sprinklers
                 l_best, r_best = l, r
 
-        # current sprinkler uncompatible with previously used spinkler
+        # current sprinkler is uncompatible with previously used sprinkler
         else:
-            # check if current sprinkler is compatible with the best sprinkler
+            # current sprinkler is compatible with the best sprinkler
             if l <= r_best:
-                # best sprinkler gets included in the solution
                 s += 1
                 l_used, r_used = l_best, r_best
-
-                # current sprinkler becomes the new best
                 l_best, r_best = l, r
 
-            # unbreachable gap between sprinklers
+            # current sprinkler is uncompatible - unbreachable gap
             else:
-                # print("Unbreachable gap between sprinklers")
                 print(-1)
                 break
 
-    # loop
-    # check if first sprinkler can cover the left side of the strip,
-    # no - abort
-    # yes - continue
 
-    # check if that sprinkler covers the right side of the strip
-    # yes - finish and output number of sprinklers
-    # no - continue
+"""
+    Previous approach
 
-    # check if that sprinkler is compatible with the previous one
-    # yes - check if it covers the most area to the right
-    # yes - store information about that sprinkler
-    # no - check if it is compatible with the best sprinkler
-    # yes - additional sprinkler needed
-    # no - gap between sprinklers, abort
+    # start with no sprinklers, on the left side of the strip
+    s = 1
+    left, right = coverage.pop(0)
+    if left > 0:
+        print(-1)
+        break
+
+    # choose the sprinkler overlapping with previous one, with the biggest coverage
+    while coverage:
+
+        # end was reached, finish
+        if right >= end:
+            print(s)
+            break
+
+        # choose compatible sprinklers, do not include yourself
+        compatible = [
+            [l, r] for l, r in coverage if l <= right and l != left and r != right
+        ]
+
+        # detect gap and finish or proceed
+        if compatible:
+            left, right = max(compatible, key=lambda x: x[1])
+            f = coverage.index([left, right])
+            coverage = coverage[f:]
+            s += 1
+        else:
+            print("-1")
+            break
+    """
